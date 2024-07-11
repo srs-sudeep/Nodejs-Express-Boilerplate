c55ae7567fa0a025d2607e1fa4dde8c5ab7333cb
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-07-11 22:40:11 +0530

    chore: added dependecies linting githooks and commitlinters

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    ignores: ['bin', '.husky', 'commitlint.config.js', 'eslint.config.js', 'node_modules'],
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello via Bun!");
+console.log('Hello via Bun!');
+console.log('Hello via Bun!');
+console.log('Hello via Bun!');
