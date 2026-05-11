30c11f0445e6c73b8e0896b6a0462a456d02c910
darishkhan <darishkhanon155@gmail.com>
2026-05-11 22:55:00 +0900

    fix(package.json): updatd command for commit

--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -18,7 +18,7 @@ const paginate = (schema) => {
    * @returns {Promise<QueryResult>}
    */
   schema.statics.paginate = async function (filter, options) {
-    let sort = '';
+    let sort;
     if (options.sortBy) {
       const sortingCriteria = [];
       options.sortBy.split(',').forEach((sortOption) => {
